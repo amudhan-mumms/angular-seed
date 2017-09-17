435f4fcfbdeece69f1e49018432e1f28b9d6edd9
amudhan-mumms <amudhan.a@mumms.com>
2017-09-17 19:47:09 +0530

    Adding angular-strap, time from and duration

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,13 +2,35 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
+.config(['$routeProvider', function($routeProvider, $timepickerProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
   });
+
+  
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', function($scope) {
+	var id = 0;
+	$scope.addClient = function() {
+  		var clientId = id++;
+  		var client = {};
+  		$scope.client.id = clientId++;
+  		console.log("***** Details *****:");
+  		console.log("Name: "+$scope.client.name);
+  		console.log("Name: "+$scope.client.age);
+  		console.log("Name: "+$scope.time);
+  		console.log("Name: "+$scope.duration);
+  		var time = moment($scope.time);
+  		var dur = moment($scope.duration);
+  		console.log("time isvalid "+time.isValid());
+  		console.log("time : hour "+time.get('hour'));
+  		var durResult = time.add(dur.get('hour'),'hour');
+  		durResult = durResult.add(dur.get('minutes'),'minutes');
+  		console.log("end time "+durResult.get('hour')+":"+durResult.get('minutes'))
 
-}]);
+	}
+	
+
+});
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -9,6 +9,32 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', [function() {
+.controller('View2Ctrl', function($scope) {
+	$scope.clickMe = function (){
+  		f2(30);
+  		console.log("Inside click me ");
+  		var a = 1;
+  		function f1() {
+			a = 10;
+			return;
+  		}
+  		f1();
+  		console.log("a is: "+a);
+  		var f = function(b){
+    		console.log("b is :"+b);
+  		}
+  		f(20);
+  		f2(30);
+  		function f2(c){
+    		console.log("Function expression");
+    		console.log("c is "+c);
+  		}
+  		var d = 1;//global scope
+		function f2 () {
+    		var d = 10; // local scope
+		}
+		f2();
+	 	console.log("d is :"+d);
+	}
 
-}]);
+});
